398a477441e1d8308dbddef35d2c5667eca6bfb1
yaroslavkuzyk <y.kuzyk@wpt.dev>
2023-01-31 00:53:03 +0200

    add size in progress bar

--- components/ProgressBar.vue
+++ components/ProgressBar.vue
@@ -1,63 +1,56 @@
 <template>
   <div>
-     <div class="progress-wrap" v-for="item in progressObject">
-    <div class="progress-line" >
-      <div class="progress-persent" >
-       <div class="progress-text" >
-        <h3>
-          {{item.name}}
-        </h3>
-        <h3>
-          {{item.size}}
-        </h3>
+    <div class="progress-wrap" v-for="item in progressObject">
+      <div class="progress-line">
+        <div class="progress-persent" :style="{ width: item.size }">
+          <div class="progress-text">
+            <h3>
+              {{ item.name }}
+            </h3>
+            <h3>
+              {{ item.size }}
+            </h3>
+          </div>
+        </div>
       </div>
     </div>
-    </div>
-  </div>
   </div>
 </template>
-  
+
 <script>
 export default {
-  name: 'ProgressBar',
-  
+  name: "ProgressBar",
+
   data() {
-    return{
-      progressObject:[
-      {name:'BRANDING', size: '90%'},
-      {name:'DESIGN', size: '80%'},
-      {name:'DEVELOPMENT', size: '85%'},
+    return {
+      progressObject: [
+        { name: "BRANDING", size: "90%" },
+        { name: "DESIGN", size: "80%" },
+        { name: "DEVELOPMENT", size: "85%" },
       ],
-      sizeObject:{
-      BRANDING: '90px',
-      DESIGN: 80,
-      DEVELOPMENT: 85
-    }
-    }
-  }
-  
-}
+    };
+  },
+};
 </script>
-  
-<style lang="scss" >
-.progress-wrap{
+
+<style lang="scss">
+.progress-wrap {
   width: 100%;
   position: relative;
   margin-top: 40px;
 }
-.progress-line{
+.progress-line {
   width: 370px;
   height: 2px;
-  background: #F0F0F0;
+  background: #f0f0f0;
 }
-.progress-persent{
+.progress-persent {
   position: absolute;
   height: 2px;
-  background: #1D1D1D;
+  background: #1d1d1d;
   top: 0;
-  width: v-bind(sizeObject);
 }
-.progress-text{
+.progress-text {
   display: flex;
   justify-content: space-between;
 }
